3a53883ef1a8a0ddc9084ecd58f6e400e4ab1814
Andy Race <Andy.Race@willistowerswatson.com>
2019-11-04 10:41:44 +0000

    File|New Project ASP.Net Core MVC project with no authentication

---
+++ wtw.cet.afd.webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AFD_WebApp.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
